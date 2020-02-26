9b56e9bd1959c961bbc1e79b6d155f944b8d266e
Joshua Beers <beerman.develops@gmail.com>
2020-02-26 12:45:04 -0600

    Create Fake Data Service to Simulate Database connection. Create MainViewModel and ViewModelBase that implements INotifyPropertyChanged Create Basic UI Elements in MainWindow Create Bootstrap Dependency Injection

---
+++ FriendOrganizer.UI/Data/IFriendDataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public interface IFriendDataService
+    {
+        IEnumerable<Friend> GetAll();
+    }
+}
---
+++ FriendOrganizer.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Navigation;
+using FriendOrganizer.UI.Data;
+using Model;
+
+namespace FriendOrganizer.UI.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private IFriendDataService _friendDataService;
+        private Friend _selectedFriend;
+
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        public Friend SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends = new ObservableCollection<Friend>();
+            _friendDataService = friendDataService;
+
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+
+            Friends.Clear();
+
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+    }
+}
